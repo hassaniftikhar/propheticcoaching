ad06720375fd24c8996172a68ac25655e60bfb1e
Hassan Iftikhar <hassaniftikhar@HassansMac.local>
2013-12-07 15:17:29 +0500

    initial commit

---
+++ propheticcoaching/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Propheticcoaching::Application.config.secret_key_base = '238b32b2f8e959a58fc35cc28a51b7d47a3c630c7a1d8790aa73d033089c3a5727ee7ae7e40d3c39778a3a86466eaeb7c39b4ada7375b0c3ecbe25f311962c9c'
