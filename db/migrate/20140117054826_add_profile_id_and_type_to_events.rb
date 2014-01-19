6e5ef3d3627929a43063f3f789fb5b42a00d929e
Hassan Iftikhar <hassan.iftikhar.ch@gmail.com>
2014-01-20 00:20:33 +0500

    merging imrans work and adjusting code

--- db/migrate/20140117054826_add_profile_id_and_type_to_events.rb
+++ db/migrate/20140117054826_add_profile_id_and_type_to_events.rb
@@ -1,7 +1,7 @@
 class AddProfileIdAndTypeToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :profile_id, :string, :null => false
-    add_column :events, :profile_type, :string, :null => false
+    add_column :events, :profile_id, :string
+    add_column :events, :profile_type, :string
     remove_column :events, :mentee_id
   end
 end
