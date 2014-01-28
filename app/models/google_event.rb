4609385d88bfeeb445393d5ad285417f3389c6fd
imran <ijmalik@gmail.com>
2014-01-28 18:56:53 +0500

    URL validation

--- app/models/google_event.rb
+++ app/models/google_event.rb
@@ -1,3 +1,16 @@
+
 class GoogleEvent < ActiveRecord::Base
   belongs_to :profile, :polymorphic => true
+  #validates :url, :eventAddress
+  validate :eventAddress
+  def eventAddress
+  		URI.parse(url)
+
+    # begin
+    #   URI.parse(url)
+    # rescue 
+    # 		p URI::InvalidURIError
+    # 		return false
+    #end
+		end
 end
