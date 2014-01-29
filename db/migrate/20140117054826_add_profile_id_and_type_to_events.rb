797b8eaa702cfa94766cdd03810cdecb8bf9a605
Hassan Iftikhar <hassan.iftikhar.ch@gmail.com>
2014-01-29 22:42:56 +0500

    setting time format

--- db/migrate/20140117054826_add_profile_id_and_type_to_events.rb
+++ db/migrate/20140117054826_add_profile_id_and_type_to_events.rb
@@ -2,6 +2,9 @@ class AddProfileIdAndTypeToEvents < ActiveRecord::Migration
   def change
     add_column :events, :profile_id, :string
     add_column :events, :profile_type, :string
+
+    Event.all.each{|e| e.profile_id=e.mentee_id; e.profile_type='mentee';e.save! }
+
     remove_column :events, :mentee_id
   end
 end
