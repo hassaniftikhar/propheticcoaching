634ba630cd893f8c54ebad7b0135ccc578137bb1
Hassan Iftikhar <hassan.iftikhar.ch@gmail.com>
2014-03-03 18:50:42 +0500

    events fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   # before_action :set_mentee_id  
   def index
   		@latest_resources = Ebook.order("created_at desc").limit(5)
-  		@coach_events = @current_user.events.where("endtime >= ?", Time.now ).order("starttime asc")
+  		@coach_events = @current_user.events.where("endtime >= ?", Time.now ).order("starttime asc") if @current_user.events
   		@user    = @current_user
     @mentees = @user.mentees.page params[:page]
   end
