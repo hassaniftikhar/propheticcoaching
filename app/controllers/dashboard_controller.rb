e8d12589bf64abddbcb4a446729cb389c6315bd0
imran <ijmalik@gmail.com>
2014-03-17 11:13:13 +0500

    authneticate dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,12 @@
 class DashboardController < ApplicationController
   # before_action :set_calendar_properties
   # before_action :set_mentee_id  
+  before_action :authenticate_user!
   def index
-  		if @current_user
-		  		@latest_resources = Ebook.order("created_at desc").limit(5)
-		  		@coach_events = @current_user.events.where("endtime >= ?", Time.now ).order("starttime asc")
-		  		@user    = @current_user
-		    @mentees = @user.mentees.page params[:page]
-		  else
-		  	 redirect_to new_user_session_path
-	   end
+  		@latest_resources = Ebook.order("created_at desc").limit(5)
+  		@coach_events = @current_user.events.where("endtime >= ?", Time.now ).order("starttime asc")
+  		@user    = @current_user
+    @mentees = @user.mentees.page params[:page]
   end
 
 end
