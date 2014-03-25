45d801ef702e5b14a1593c90ea3ec496df962443
imran <ijmalik@gmail.com>
2014-03-25 18:48:24 +0500

    email resque error removed

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,12 +3,11 @@ class Goal < ActiveRecord::Base
   belongs_to :mentee
 
 
-  def deliver_email
+  def deliver_email(current_user)
     email = self.mentee.email_histories.new
     email.body = self.body
-
     if email.save
-      Resque.enqueue(EmailSend, email.id)
+      Resque.enqueue(EmailSend, email.id, current_user.id)
     end
   end
 
