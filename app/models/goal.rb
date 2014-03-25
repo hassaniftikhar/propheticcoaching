e867973e98b6f438bfd4d28b0c2569e1e1e929cd
Hassan Iftikhar <hassan.iftikhar.ch@gmail.com>
2014-03-25 12:46:10 +0500

    adjusting goals for delevering emails

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,15 @@
 class Goal < ActiveRecord::Base
 
   belongs_to :mentee
+
+
+  def deliver_email
+    email = self.mentee.email_histories.new
+    email.body = self.body
+
+    if email.save
+      Resque.enqueue(EmailSend, email.id)
+    end
+  end
+
 end
