fec6fd6fa9c23f0f218c5dcdaf4586598dc9ce3b
imran <ijmalik@gmail.com>
2014-04-17 16:32:46 +0500

    heroku deployment redis v9

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,10 @@
-$redis = Redis.new
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+
+
+# uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
+# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+
+# $redis = Redis.new
+
