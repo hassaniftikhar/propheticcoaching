c80c45c09281ec99ac30be571574f76ff14e4565
imran <ijmalik@gmail.com>
2014-04-25 15:47:00 +0500

    resque ok on heroku v8

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,5 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+# uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 # REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
