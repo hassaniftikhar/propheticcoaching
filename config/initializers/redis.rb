20f23e001566d6061d268fd3c51240e8536f92b3
imran <ijmalik@gmail.com>
2014-04-25 14:52:12 +0500

    resque v3

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,9 @@
 # uri = URI.parse(ENV["REDISTOGO_URL"])
 uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
-REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+# REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
 
 
-# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 
 # $redis = Redis.new
