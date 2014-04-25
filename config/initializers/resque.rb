f39ea054c3ed1a79c1c44fea4ede6bb8c36c3e7d
imran <ijmalik@gmail.com>
2014-04-25 15:29:23 +0500

    resque error testing on heroku v7

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,6 @@
-uri = URI.parse ENV["REDISCLOUD_URL"]
-Resque.redis = Redis.new host:uri.host, port:uri.port, password:uri.password
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+# Resque.redis = Redis.new host:uri.host, port:uri.port, password:uri.password
