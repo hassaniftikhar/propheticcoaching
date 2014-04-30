672654fff50515dd84b009737a819d5798937ed3
imran <ijmalik@gmail.com>
2014-04-30 11:33:30 +0500

    integrate dsn sign_up page

---
+++ spec/factories/time_slots.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :time_slot do
+    starttime "2014-03-26 12:01:32"
+    endtime "2014-03-26 12:01:32"
+  end
+end
---
+++ spec/views/time_slots/create.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "time_slots/create.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
