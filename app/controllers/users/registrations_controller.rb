508c6f5a1dc814c8b4d7be93de19ec3739ef326b
imran <ijmalik@gmail.com>
2014-05-06 16:46:13 +0500

    integrate dsn static pages v3

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,5 +5,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name, :address, :home_phone, :availablity_time, :best_time_to_call, :date_of_birth)
+    devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name, :address, :home_phone, :availablity_time, :best_time_to_call, :date_of_birth, :remember_me)
   end
 end
