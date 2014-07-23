1e221b3231f46649330fe31aa05288736365cf82
imran <ijmalik@gmail.com>
2014-07-23 11:56:05 +0500

    title length

--- app/models/best_feature.rb
+++ app/models/best_feature.rb
@@ -4,6 +4,9 @@ class BestFeature < ActiveRecord::Base
 	process_in_background :image
 
 	validates_presence_of :title
-  validates_presence_of :image
-  validates_uniqueness_of :title, :case_sensitive => false
+ validates :title, length: { maximum: 26,
+    too_long: "%{count} characters is the maximum allowed" }	
+ validates_presence_of :image
+ validates_uniqueness_of :title, :case_sensitive => false
+ # validates_length_of :title, :minimum => 5, :maximum => 30, :allow_blank => false
 end
