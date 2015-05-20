b4a3c2fbd57a7f94dc2463e580c4de4013dd83b0
imran <ijmalik@gmail.com>
2015-05-20 12:17:14 +0500

    video back

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -3,13 +3,13 @@ class InfoController < ApplicationController
     @benefits = Benefit.order("updated_at desc").limit(10)
     @featured_products = FeaturedProduct.order("updated_at desc").limit(20)
     @best_features = BestFeature.order("updated_at desc").limit(4)
-    # @video_path = '<iframe src="//player.vimeo.com/video/100508776" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'.html_safe
-    # @video_info = Video.order("updated_at desc").first.path
-    # if @video_info
-    #   path_start = @video_info.index("<iframe")
-    #   path_end = @video_info.index("</iframe>")+9
-    #   @video_path = @video_info.slice(path_start, path_end).html_safe
-    # end
+    @video_path = '<iframe src="//player.vimeo.com/video/100508776" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'.html_safe
+    @video_info = Video.order("updated_at desc").first.path
+    if @video_info
+      path_start = @video_info.index("<iframe")
+      path_end = @video_info.index("</iframe>")+9
+      @video_path = @video_info.slice(path_start, path_end).html_safe
+    end
 
     # @video_path = Video.order("updated_at desc").first.path.html_safe
   end
